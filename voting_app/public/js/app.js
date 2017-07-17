998e104578295462d1a7c99733d1c1cbd5ac2685
OmarSagazio <omar.sagazio@gmail.com>
2017-07-17 09:08:06 +0200

    1st component

---
+++ voting_app/public/js/app.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+class ProductList extends React.Component {
+    render {
+        return (
+            <div className='ui unstackable items'>
+                ProductList Component
+            </div>
+        );
+    }
+}
